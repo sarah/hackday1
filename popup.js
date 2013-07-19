f5d813b9eac62853a8f07dea8e0338189ff757e3
Sarah Gray <sarahg.gray@gmail.com>
2013-07-19 15:29:07 -0500

    try adding phono locally

--- popup.js
+++ popup.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  var call_link = $("a#call");
+  console.log("call_link " + call_link);
+   
+  $(call_link).click(function(e) {
+    e.preventDefault();
+    console.log("Handler for .click() called.");
+  });
+
+
+
   chrome.tabs.getSelected(null, function(tab) { 
   chrome.tabs.sendMessage(tab.id, {method: "getNumber"}, function(response){
     if(response.method == "getNumber"){
-      var number = response.data;
+      var number = response.data.replace("-", "");
       var prefix = number.substr(0,3);
       var extension = number.substr(3,3);
       var last_four = number.substr(6,4);
