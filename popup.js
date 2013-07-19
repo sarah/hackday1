5ad6abe7bba0c0e53ee03803813d5e086040a394
Sarah Gray <sarahg.gray@gmail.com>
2013-07-19 14:52:34 -0500

    add phono file

--- popup.js
+++ popup.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.sendMessage(tab.id, {method: "getNumber"}, function(response){
     if(response.method == "getNumber"){
       var number = response.data;
-      $("#number").text(number);
+      var prefix = number.substr(0,3);
+      var extension = number.substr(3,3);
+      var last_four = number.substr(6,4);
+      var displayed_number = "(" + prefix + ")-" + extension + "-"  + last_four;
+
+      $("#number").text(displayed_number);
     }
   });
 
